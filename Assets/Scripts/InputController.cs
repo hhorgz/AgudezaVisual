1d27f7db6dbb218d7c2a4cff3c6dc97d1ef9e110
Hanns Guzman <hanns.guzman@gmail.com>
2019-04-02 21:57:54 -0600

    Se configura la transicion de 2D a VR

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class InputController : MonoBehaviour {
+
+	public bool IsActionButtonPressed {
+		get {
+			// 8BitDo: A
+			if (Input.GetKeyUp (KeyCode.JoystickButton0)) {
+				Debug.Log ("KeyUp: " + KeyCode.JoystickButton0.ToString ());
+				return true;
+			}
+			// 8BitDo: B
+			else if (Input.GetKeyUp (KeyCode.JoystickButton1)) {
+				Debug.Log ("KeyUp: " + KeyCode.JoystickButton1.ToString ());
+				return true;
+			}
+
+			// 8BitDo: X
+			else if (Input.GetKeyUp (KeyCode.JoystickButton2)) {
+				Debug.Log ("KeyUp: " + KeyCode.JoystickButton2.ToString ());
+				return true;
+			}
+
+			// 8BitDo: Y
+			else if (Input.GetKeyUp (KeyCode.JoystickButton3)) {
+				Debug.Log ("KeyUp: " + KeyCode.JoystickButton3.ToString ());
+				return true;
+			}
+
+
+			// 8BitDo: Left Dpad
+			else if (Input.GetKeyUp (KeyCode.LeftArrow)) {
+				Debug.Log ("KeyUp: " + KeyCode.LeftArrow.ToString ());
+				return true;
+			}
+
+			// 8BitDo: Right Dpad
+			else if (Input.GetKeyUp (KeyCode.RightArrow)) {
+				Debug.Log ("KeyUp: " + KeyCode.RightArrow.ToString ());
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
