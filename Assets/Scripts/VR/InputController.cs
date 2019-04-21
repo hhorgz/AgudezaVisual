537654b0766ee397db7ba28e0a2e8438a1bcfcce
Hanns Guzman <hanns.guzman@gmail.com>
2019-04-21 00:04:06 -0600

    prototipo para cambio de material

---
+++ Assets/Scripts/VR/InputController.cs
@@ -0,0 +1,57 @@
+﻿namespace AgudezaVisual.VR
+{
+
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using UnityEngine.XR;
+
+	public class InputController : MonoBehaviour
+	{
+		/// <summary>
+		/// Gets a value indicating whether a button has been pressed.
+		/// </summary>
+		/// <value><c>true</c> if a button has benn pressed; otherwise, <c>false</c>.</value>
+		public bool IsActionButtonPressed {
+			get {
+				// 8BitDo: A
+				if (Input.GetKeyUp (KeyCode.JoystickButton0)) {
+					Debug.Log ("KeyUp: " + KeyCode.JoystickButton0.ToString ());
+					return true;
+				}
+			// 8BitDo: B
+			else if (Input.GetKeyUp (KeyCode.JoystickButton1)) {
+					Debug.Log ("KeyUp: " + KeyCode.JoystickButton1.ToString ());
+					return true;
+				}
+
+			// 8BitDo: X
+			else if (Input.GetKeyUp (KeyCode.JoystickButton2)) {
+					Debug.Log ("KeyUp: " + KeyCode.JoystickButton2.ToString ());
+					return true;
+				}
+
+			// 8BitDo: Y
+			else if (Input.GetKeyUp (KeyCode.JoystickButton3)) {
+					Debug.Log ("KeyUp: " + KeyCode.JoystickButton3.ToString ());
+					return true;
+				}
+
+
+			// 8BitDo: Left Dpad
+			else if (Input.GetKeyUp (KeyCode.LeftArrow)) {
+					Debug.Log ("KeyUp: " + KeyCode.LeftArrow.ToString ());
+					return true;
+				}
+
+			// 8BitDo: Right Dpad
+			else if (Input.GetKeyUp (KeyCode.RightArrow)) {
+					Debug.Log ("KeyUp: " + KeyCode.RightArrow.ToString ());
+					return true;
+				}
+
+				return false;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/VR/OptotipoController.cs
@@ -0,0 +1,68 @@
+﻿namespace AgudezaVisual.VR
+{
+	
+
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	[RequireComponent (typeof(Collider))]
+	public class OptotipoController : MonoBehaviour
+	{
+		/// Listens to controller's input
+		private InputController inputController;
+		/// Indicates if the object is being gazed
+		private bool gazedAt;
+
+
+		public Material inactiveMaterial;
+		public Material gazedAtMaterial;
+
+		Renderer render;
+
+
+		// Use this for initialization
+		void Start ()
+		{
+			inputController = new InputController ();
+			render = GetComponent<Renderer> ();
+
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+			if (this.gazedAt && inputController.IsActionButtonPressed) {
+				// Check if the optotype selected is the correct one
+				CheckAnswer ();
+			}
+		}
+
+		/**
+		 * It defines the behavior of an object when this is gazed or not.
+		 * - If gazedAt == true the object's material will be switched to gazedAtMaterial
+		 * - If gazedAt == false the object's material will be switched to inactiveMaterial
+		 */
+		public void SetGazedAt (bool gazedAt)
+		{
+			this.gazedAt = gazedAt;
+			if (inactiveMaterial != null && gazedAtMaterial != null) {
+				render.material = gazedAt ? gazedAtMaterial : inactiveMaterial;
+				return;
+			}
+		}
+
+		/// <summary>
+		/// Checks the answer.
+		/// </summary>
+		/// <returns><c>true</c>, if answer was checked, <c>false</c> otherwise.</returns>
+		public void CheckAnswer ()
+		{
+			Debug.Log ("===== checkAnswer =====");
+			Debug.Log (render.material.name);
+			Material material = Resources.Load ("Materials/e_snellen", typeof(Material)) as Material;
+			render.material = material;
+		}
+
+	}
+}
