c960e8e1ffff7000adb7ba2618da0274706fc3d6
Hanns Guzman <hanns.guzman@gmail.com>
2019-04-29 23:58:13 -0600

    Generacion aleatoria de optotipos de snellen con materiales incluidos

--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -1,22 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using AgudezaVisual.Configuracion;
 
-public class Jugador {
-//	private string _nombre = null;
-//	private int _edad = 0;
-	private string _nombre = "dummy";
-	private int _edad = 100;
+public class Jugador
+{
 
-	public static Jugador jugador = new Jugador();
+	public static Jugador jugador = new Jugador ();
+
+	private string _nombre;
+	private int _edad;
+
+	public Jugador () {
+		_nombre = "dummy";
+		_edad = 10;
+	}
+
 	public string Nombre {
 		get { return _nombre; }
 	}
+
 	public int Edad {
 		get{ return _edad; }
 	}
+
+	public DificultadEnumerator Dificultad {
+		get {
+			if (_edad < 5) { 
+				return DificultadEnumerator.OPTOTIPOS_LEIA;
+			} else {
+				return DificultadEnumerator.OPTOTIPOS_SNELLEN;
+			}
+		}
+	}
+
 	public bool IsEmpty {
-		get{
+		get {
 			if (string.IsNullOrEmpty (Nombre) || Edad == 0) {
 				return true;
 			} else {
@@ -25,11 +44,12 @@ public class Jugador {
 		}
 	}
 
-	public bool setDatos(string nombre, int edad){
+	public bool setDatos (string nombre, int edad)
+	{
 		if (string.IsNullOrEmpty (nombre) || edad <= 0) {
 			return false;
 		} else {
-			_nombre = nombre.ToUpper();
+			_nombre = nombre.ToUpper ();
 			_edad = edad;
 			return true;
 		}
@@ -37,6 +57,6 @@ public class Jugador {
 
 	public override string ToString ()
 	{
-		return string.Format ("[Jugador: Nombre={0}, Edad={1}, IsEmpty={2}]", Nombre, Edad, IsEmpty);
+		return string.Format ("[Jugador: Nombre={0}, Edad={1}, IsEmpty={2}, Dificultad={3}]", Nombre, Edad, IsEmpty, Dificultad);
 	}
 }
---
+++ Assets/Scripts/VR/OptotipoFactory.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AgudezaVisual.Configuracion;
+
+namespace AgudezaVisual.VR
+{
+	public class OptotipoFactory : MonoBehaviour
+	{
+		
+		private OptotipoEnum[] optotipos;
+		public List<GameObject> opciones;
+
+		public void Start ()
+		{
+//			Renderer render = opcion1.GetComponent<Renderer> ();
+//			Material material = Resources.Load ("Materials/e_snellen", typeof(Material)) as Material;
+//			render.material = material;
+//
+//			OptotipoController controller = opcion1.GetComponent<OptotipoController> ();
+//			controller.inactiveMaterial = material;
+//			controller.gazedAtMaterial = Resources.Load ("Materials/e_snellen_seleccionado", typeof(Material)) as Material;
+			DefinirOpciones ();
+		}
+
+		private void SetOpcionesVR ()
+		{ 
+			
+		}
+
+		/**
+		 * Opcion que retorna un numero aleatorio asociado con el nombre del optotipo
+		 */
+		private OptotipoEnum ObtenerOptotipoAleatorio ()
+		{
+			int rangoInferior = -1;
+			int rangoSuperior = -1;
+			int optotipoAleatorio;
+			Random random = new Random ();
+
+			// Definir el rango de la funcion aleatorio dependiendo del tipo de optotipos
+			if (Jugador.jugador.Dificultad == DificultadEnumerator.OPTOTIPOS_LEIA) {
+				rangoInferior = (int)OptotipoEnum.cruz_leia;
+				rangoSuperior = (int)OptotipoEnum.estrella_leia_seleccionado;
+			} else if (Jugador.jugador.Dificultad == DificultadEnumerator.OPTOTIPOS_SNELLEN) {
+				rangoInferior = (int)OptotipoEnum.c_snellen;
+				rangoSuperior = (int)OptotipoEnum.z_snellen_seleccionado;
+			}
+
+			// Obtener numero aleatorio
+			optotipoAleatorio = Random.Range (rangoInferior, rangoSuperior);
+
+			// Si el numero no es par quiere decir que es un optotipo de tipo seleccionado
+			if (optotipoAleatorio % 2 != 0) {
+				optotipoAleatorio--;
+			}
+			return (OptotipoEnum)optotipoAleatorio;
+		}
+
+
+		/**
+		 * Funcion que asigna de manera aleatoria los valores de las opciones que estaran disponibles
+		 */
+		public void DefinirOpciones ()
+		{
+			// Definir la cantidad de opciones en base a la dificultad
+			if (Jugador.jugador.Dificultad == DificultadEnumerator.OPTOTIPOS_LEIA) {
+				optotipos = new OptotipoEnum[4];
+			} else if (Jugador.jugador.Dificultad == DificultadEnumerator.OPTOTIPOS_SNELLEN) {
+				optotipos = new OptotipoEnum[5];
+			}
+
+			// Obtener un valor aleatorio  para cada opcion
+			for (int i = 0; i < optotipos.Length; i++) {
+				OptotipoEnum optotipoAleatorio = ObtenerOptotipoAleatorio ();
+
+				// Verificar si la opcion aun no existe
+				if (!YaExisteOpcion (optotipoAleatorio)) {
+					optotipos [i] = optotipoAleatorio;
+					AsignarMateriales (opciones[i], optotipoAleatorio);
+				} else {
+					// Si la funcion ya existe, se intentara nuevamente
+					i--;
+					continue;
+				}
+			}
+		}
+
+		/**
+		 * Compara si el OptotipoEnum que recibe como parametro ya 
+		 * existe dentro del arreglo optotipos
+		 */
+		public bool YaExisteOpcion (OptotipoEnum opcion)
+		{
+			for (int i = 0; i < optotipos.Length; i++) {
+				if (optotipos [i] == opcion)
+					return true;
+			}
+			return false;
+		}
+
+		/**
+		 * Cargar los materiales asociados al  OptotipoEnum y asignarlos al controlador de la opcion
+		 */
+		public void AsignarMateriales (GameObject opcion, OptotipoEnum optotipo)
+		{
+			// Obtenemos el controlador y el rederizador
+			OptotipoController controller = opcion.GetComponent<OptotipoController> ();
+			Renderer render = opcion.GetComponent<Renderer> ();
+
+			// Material cuando no esta siendo observado
+			Material inactiveMaterial = Resources.Load ("Materials/" + optotipo, typeof(Material)) as Material;
+			controller.inactiveMaterial = inactiveMaterial;
+			render.material = inactiveMaterial;
+
+			// Material cuando esta siendo observado
+			Material gazedAtMaterial = Resources.Load ("Materials/" + optotipo + "_seleccionado", typeof(Material)) as Material;
+			controller.gazedAtMaterial = gazedAtMaterial;
+		}
+	}
+}
